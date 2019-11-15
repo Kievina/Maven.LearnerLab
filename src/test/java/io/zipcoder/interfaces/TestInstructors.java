2c937a23e907f6118c06b16626a76fb1da8d8de1
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-15 11:27:32 -0500

    complete test instructors and students

--- src/test/java/io/zipcoder/interfaces/TestInstructors.java
+++ src/test/java/io/zipcoder/interfaces/TestInstructors.java
@@ -8,26 +8,26 @@ public class TestInstructors {
     @Test
     public void testGetInstance1() {
         Instructors instructors = Instructors.getInstance();
-        int expected = 2;
-        int actual = instructors.personList.size();
         Long expected1 = 14L;
         Long expected2 = 15L;
-        instructors.personList.get(0).getId().equals(expected1);
-        instructors.personList.get(1).getId().equals(expected2);
-        Assert.assertTrue(instructors.personList.get(0).getId().equals(14L));
-        Assert.assertTrue(instructors.personList.get(1).getId().equals(15L));
 
+        Long actual1 = instructors.personList.get(0).getId();
+        Long actual2 = instructors.personList.get(1).getId();
+
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
     }
 
     @Test
     public void testGetInstance2() {
         Instructors instructors = Instructors.getInstance();
-        int expected = 2;
-        int actual = instructors.personList.size();
         String expected1 = "First";
         String expected2 = "Second";
 
-        Assert.assertTrue(instructors.personList.get(0).getName().equals("First"));
-        Assert.assertTrue(instructors.personList.get(1).getName().equals("Second"));
+        String actual1 = instructors.personList.get(0).getName();
+        String actual2 = instructors.personList.get(1).getName();
+
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
     }
 }
